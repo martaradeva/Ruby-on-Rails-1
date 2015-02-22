4a28248b0815750eb63af9fb2c306679fc7669bd
Martina Radeva <martaradeva@yahoo.com>
2015-02-22 21:48:29 +0200

    Optional working

--- Application/1-Optional/optional.rb
+++ Application/1-Optional/optional.rb
@@ -2,7 +2,7 @@
 # Kate, I owe you a beer.
 
 class Optional < BasicObject
-  instance_methods.each { |m| undef_method m unless m =~ /(^__send__$|^object_id$)/ }
+  # instance_methods.each { |m| undef_method m unless m =~ /(^__send__$|^object_id$)/ }
 
   attr_accessor :argument
 
@@ -12,29 +12,10 @@ class Optional < BasicObject
 
   def value
     @argument
-    # ::Object::Proc.new { |object| object.argument }
   end
 
   def method_missing(name, *args, &block)
-    # puts "pass one"
-    #self
-    # @argument.send(name, *args, &block)
+    return ::Object::Optional.new(nil) if @argument == nil
     ::Object::Optional.new( @argument.send(name, *args, &block))
-    # args
-    # ::Object::Proc.new { |object| object.send(name, *args, &block) }
   end
 end
-
-# class Proxy
-#   instance_methods.each { |m| undef_method m unless m =~ /(^__send__$|^object_id$)/ }
-
-#   def method_missing(name, *args, &block)
-#     Proc.new { |object| object.send(name, *args, &block) }
-#   end
-
-#   def to_proc
-#     Proc.new { |object| object }
-#   end
-# end
-
-# P = Proxy.new

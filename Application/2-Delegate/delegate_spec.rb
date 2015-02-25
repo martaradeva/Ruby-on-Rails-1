979e4113e176ac20710c8a2db15e5786dfd04e24
Martina Radeva <martaradeva@yahoo.com>
2015-02-25 11:09:25 +0200

    Specs for task 02

--- Application/2-Delegate/delegate_spec.rb
+++ Application/2-Delegate/delegate_spec.rb
@@ -7,12 +7,13 @@ describe 'Module#delegate' do
     before (:each) do
       User = Struct.new(:first_name, :last_name)
 
-      class Invoce
-        delegate :fist_name, :last_name, to: :'@user'
+      class Invoce < Struct.new(:user)
+        delegate :first_name, :last_name, to: :'user'
 
-        def initialize(user)
-          @user = user
-        end
+        # def initialize(user)      # if this initialize method is present then
+        #   @user = User            # invoice = Invoce.new(user) initiates
+        # end                       # invoice with user = nil
+
       end
     end
 
@@ -20,20 +21,10 @@ describe 'Module#delegate' do
     let(:invoice) { Invoce.new(user) }
 
     it 'class instance responds to new methods' do
-      expect(invoice.respond_to? :first_name).to eq(true)
-      expect(invoice.respond_to? :last_name).to eq(true)
-    end
-
-    it 'can delegate accessors to class' do
-      # puts invoice.methods.inspect
       expect(invoice.first_name).to eq("Genadi")
       expect(invoice.last_name ).to eq("Samokovarov")
     end
 
-    it 'other test' do
-      expect(invoice.respond_to? first_name).to eq(true)
-      expect(invoice.respond_to? last_name).to eq(true)
-    end
   end
 end
 

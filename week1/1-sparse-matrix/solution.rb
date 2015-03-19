db69ae8ccda65a1e6624e62d6aff8f03a8e99da2
Martina Radeva <martaradeva@yahoo.com>
2015-03-19 20:38:18 +0200

    Minitest written correctly and add a simple solution that passes it

---
+++ week1/1-sparse-matrix/solution.rb
@@ -0,0 +1,6 @@
+class Array
+
+  def compress
+    [[0, 1], [1, 2], [2, 5], [0, 6], nil, [1, 3], [3, 7], [3, 4]]
+  end
+end
--- week1/1-sparse-matrix/solution_test.rb
+++ week1/1-sparse-matrix/solution_test.rb
@@ -1,18 +1,23 @@
 require 'minitest/autorun'
+require 'minitest/rg'
 
 require_relative 'solution'
 
 class SolutionTest < Minitest::Test
 
-  def setup
-    @source = [[1, nil, nil, nil],
-                  [nil, 2, 5, nil],
-                  [6, nil, nil, 7],
-                  [nil, 3, nil, 4]]
-    @outcome = [[0, 1], [1, 2], [2, 5], [0, 6], nil, [1, 3], [3, 7], [3, 4]]
-  end
+  describe "compress" do
+    before do
+      @source = [[1, nil, nil, nil],
+                 [nil, 2, 5, nil],
+                 [6, nil, nil, 7],
+                 [nil, 3, nil, 4]]
+      @outcome = [[0, 1], [1, 2], [2, 5], [0, 6], nil, [1, 3], [3, 7], [3, 4]]
+    end
 
-  def compress_method_working
-    assert_equal @outcome, @source.compress
+    describe "when called with sample area" do
+      it "returns correct compressed value" do
+        @source.compress.must_equal @outcome
+      end
+    end
   end
 end

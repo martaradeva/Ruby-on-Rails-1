e4c96e1d8f60615e73d73f732177f8ae6ea4699c
Martina Radeva <martaradeva@yahoo.com>
2015-03-24 17:10:36 +0200

    Minimal WIP

--- week1/1-sparse-matrix/solution.rb
+++ week1/1-sparse-matrix/solution.rb
@@ -1,32 +1,19 @@
 class Array
 
-  def compress
-    # you can do this with map later
-    output = []
-    self.each do |row|
-      output << row.serialize_elements.strip 
-    end
-    output
-  end
-
 # First, take each row of the matrix, flatten each element and strip it from starting/ending nils
 # Then, until the matrix-length is 1, we put the 2nd element in the first
 # put - for each available slot in the matrix check if our element fits - if so, we write it down. 
 
+  def compress
+    map { |row| row.serialize_elements.strip }
+  end
+
   def serialize_elements
-    # you can do this with map later
-    # output = []
-    # self.each_with_index do |elem, index|
-    #   if elem then output << [index, elem]
-    #   else output << nil
-    #   end
-    # end
-    # output
-    self.each_with_index.map { |element, index| [index, element] if element }
+    each_with_index.map { |element, index| [index, element] if element }
   end
 
   def strip
-    self.drop_while{ |elem| !elem }.reverse
-        .drop_while{ |elem| !elem }.reverse
+     drop_while{ |elem| !elem }.reverse
+    .drop_while{ |elem| !elem }.reverse
   end
 end

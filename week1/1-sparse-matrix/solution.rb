d4f5e00a47767011b90a6bfd968cb47ea924479f
Martina Radeva <martaradeva@yahoo.com>
2015-03-24 16:28:47 +0200

    WIP on matrix task

--- week1/1-sparse-matrix/solution.rb
+++ week1/1-sparse-matrix/solution.rb
@@ -1,6 +1,64 @@
 class Array
 
   def compress
-    [[0, 1], [1, 2], [2, 5], [0, 6], nil, [1, 3], [3, 7], [3, 4]]
+    # new_array = apply_by_rows compress_separate_elements
+    new_array = []
+    self.each do |row|
+      new_array << row.compress_separate_elements
+    end
+    p new_array
+
+    # [[0, 1], [1, 2], [2, 5], [0, 6], nil, [1, 3], [3, 7], [3, 4]]
+  end
+
+  def apply_by_rows &block
+    # self.each do |row|
+    #   row.send
+    # end
+      # and you can do this with yield
+  end
+
+  def new_method
+    #for each empty space in self 
+    #check availability
+    #return id if all are available
+  end
+
+  def first_empty_space_id
+    self.each_with_index do |elem, index|
+      return index unless elem
+    end
+  end
+
+  def write_increment(increment, array)
+    if all_available?(increment, array) then # write this in new method
+      array.each[0] += increment
+      array.each do |index, value|
+        self[index]=value
+      end
+    end
+  end
+
+  def all_available?(start_id, array)
+    array.each_with_index do |element, index|
+      flag = element and self(start_id + index).available?
+    end
+    if flag ? true : false
+    # flag
+  end
+
+  def available?(id)
+    return true unless self[id]
+  end
+
+  def compress_separate_elements
+    # you can do this with map later
+    output = []
+    self.each_with_index do |elem, index|
+      if elem then output << [index, elem]
+      else output << nil
+      end
+    end
+    output
   end
 end
--- week1/1-sparse-matrix/solution_test.rb
+++ week1/1-sparse-matrix/solution_test.rb
@@ -5,13 +5,63 @@ require_relative 'solution'
 
 class SolutionTest < Minitest::Test
 
-  describe "compress" do
+  # describe "apply_by_rows" do
+  #   before do
+  #     @source = [[1, nil, nil, nil],
+  #                [nil, 2, 5, nil],
+  #                [6, nil, nil, 7],
+  #                [nil, 3, nil, 4]]
+
+  #     @outcome = [1, 7, 13, 7]
+  #   end
+
+  #   describe "when called" do
+  #     it "returns correct compressed value" do
+  #       @source.apply_by_rows{reduce(:+)}.must_equal @outcome
+  #     end
+  #   end
+  # end
+
+  # describe "compress_separate_elements" do
+  #   before do
+  #     @source = [1, nil, nil, nil]
+
+  #     @outcome = [[0, 1], nil, nil, nil]
+  #   end
+
+  #   describe "when called" do
+  #     it "returns correct compressed value" do
+  #       @source.compress_separate_elements.must_equal @outcome
+  #     end
+  #   end
+  # end
+
+  # describe "compress" do
+  #   before do
+  #     @source = [[1, nil, nil, nil],
+  #                [nil, 2, 5, nil],
+  #                [6, nil, nil, 7],
+  #                [nil, 3, nil, 4]]
+  #     @outcome = [[0, 1], [1, 2], [2, 5], [0, 6], nil, [1, 3], [3, 7], [3, 4]]
+  #   end
+
+  #   describe "when called with sample area" do
+  #     it "returns correct compressed value" do
+  #       @source.compress.must_equal @outcome
+  #     end
+  #   end
+  # end
+
+  describe "compress-in-progress" do
     before do
       @source = [[1, nil, nil, nil],
                  [nil, 2, 5, nil],
                  [6, nil, nil, 7],
                  [nil, 3, nil, 4]]
-      @outcome = [[0, 1], [1, 2], [2, 5], [0, 6], nil, [1, 3], [3, 7], [3, 4]]
+      @outcome = [[[0,1], nil, nil, nil],
+                 [nil, [1,2], [2,5], nil],
+                 [[0,6], nil, nil, [3,7]],
+                 [nil, [1,3], nil, [3,4]]]
     end
 
     describe "when called with sample area" do
@@ -20,4 +70,5 @@ class SolutionTest < Minitest::Test
       end
     end
   end
+
 end

63d0b892a085322a5440527bbcac84a9ef591a68
Martina Radeva <martaradeva@yahoo.com>
2015-04-03 21:16:22 +0300

    Add relation between post and comment
    Add one-to-many relation between post and comment.

---
+++ week3/1-microblog/microblog/db/migrate/20150403181110_add_comments_to_post.rb
@@ -0,0 +1,5 @@
+class AddCommentsToPost < ActiveRecord::Migration
+  def change
+    add_reference :comments, :post, index: true
+  end
+end
--- week3/1-microblog/microblog/db/schema.rb
+++ week3/1-microblog/microblog/db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403171913) do
+ActiveRecord::Schema.define(version: 20150403181110) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "post_id"
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"

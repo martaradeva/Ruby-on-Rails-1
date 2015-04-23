0b41a58bbec765b00709d7703b19c6041ebf02b1
Martina Radeva <martaradeva@yahoo.com>
2015-04-23 18:43:00 +0300

    WIP on posts controller # Please enter the commit message for your changes. Lines starting

--- week3/1-microblog/microblog/app/controllers/posts_controller.rb
+++ week3/1-microblog/microblog/app/controllers/posts_controller.rb
@@ -4,20 +4,36 @@ class PostsController < ApplicationController
   end
 
   def show
+    set_current_post
   end
 
   def new
   end
 
   def create
+    @post = params[:post]
+    @post.save
+    redirect_to index
   end
 
   def edit
+    set_current_post
   end
 
   def update
+    @post = params[:post]
+    if @post.save
+      redirect_to index
+    else #flash notice "Post couldn't be deleted"
+      render edit
+    end
   end
 
   def delete
+  end
+
+  private
+  def set_current_post
+    @post = params[:post]
   end
 end

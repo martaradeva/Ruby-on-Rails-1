6996d469649d9c4fe1ab7a684a1e7550543d809c
Martina Radeva <martaradeva@yahoo.com>
2015-04-27 21:13:30 +0300

    Add search functionality.

--- week4/store/app/controllers/searches_controller.rb
+++ week4/store/app/controllers/searches_controller.rb
@@ -1,18 +1,17 @@
 class SearchesController < ApplicationController
   def search
     set_vars_from_params
-    @search_query = find_item(@type, @property, @slug)
-    render_json(@search_query)
+    render_json(find_items)
   end
 
   private
   def set_vars_from_params
-    # @type = params[:type]
-    # @property, @slug = params[:property_slug].split('/', 2)
-    @type, @property, @slug = params[:type_property_slug].split('/')
+    @slug = params[:slug]
+    @type, @property = params[:type_property].split('/')
+    @property ||= "name"
   end
 
-  def find_item(type, property, slug)
-    Brands.first
+  def find_items
+    @type.capitalize.constantize.where("#{@property} LIKE '%#{@slug}%'")
   end
 end
--- week4/store/config/routes.rb
+++ week4/store/config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
 
   get '/brands/count', to: 'brands#count'
   resources :brands, only: [:index, :show, :create, :update, :destroy]
+  post '/brands/new', to: 'brands#create'
   get '/brands/range/:index/:count', to: 'brands#index'
   get '/brands/range/:index', to: 'brands#index'
 
   # same for categories
   # same for products
 
-  get '/search/:type_property_slug', to: 'search#search'
+  get '/search/*type_property/:slug', to: 'searches#search'
 
   root 'brands#index'
 end

a78de9e31767faf90386b4cb98d16862a3628778
Martina Radeva <martaradeva@yahoo.com>
2015-05-04 20:21:24 +0300

    WIP

--- week5/management/app/controllers/lectures_controller.rb
+++ week5/management/app/controllers/lectures_controller.rb
@@ -16,14 +16,13 @@ class LecturesController < ApplicationController
   end
 
   def create
-    create_lecture_from_params
-    @lecture.save!
+    @lecture = Lecture.create!(lecture_params)
     redirect_to lecture_url(@lecture)
   end
 
   def update
     set_lecture
-    @lecture.update!(params[:lecture].permit(:name, :body))
+    @lecture.update!(lecture_params)
     redirect_to lecture_url(@lecture)
   end
 
@@ -43,10 +42,14 @@ private
     @lecture = Lecture.find_by(id: params['id'])
   end
 
-  def create_lecture_from_params
-    @lecture = set_lecture || Lecture.new
-    @lecture.name = params["lecture"]['name']
-    @lecture.body = params["lecture"]['body']
-    @lecture
+  def lecture_params
+    params[:lecture].permit(:name, :body)
   end
+
+  # def create_lecture_from_params
+  #   @lecture = set_lecture || Lecture.new
+  #   @lecture.name = params["lecture"]['name']
+  #   @lecture.body = params["lecture"]['body']
+  #   @lecture
+  # end
 end

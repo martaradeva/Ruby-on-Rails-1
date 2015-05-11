74329e1c41621105f19a02e556f2ea9712e1a535
Martina Radeva <martaradeva@yahoo.com>
2015-05-11 21:09:16 +0300

    WIP users form

--- week7/authentication/app/models/user.rb
+++ week7/authentication/app/models/user.rb
@@ -1,18 +1,17 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
-  # users.password_hash in the database is a :string
   include BCrypt
   has_secure_password
 
-  validates :email, :first_name, :last_name, presence: true
-  # validates :password, presence: true, length: { in: 6..32 }
+  validates :first_name, :last_name, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, 
             :presence => {:message => "Enter your email address!" },
             :format => { :with => VALID_EMAIL_REGEX, :message => "Enter a valid Email address !"},
-            :uniqueness => {:case_sensitive => false, :message => "Email already exists!"}
+            :uniqueness => {:case_sensitive => false, :message => "Email already exists!"},
+            :confirmation => { :message => "Password mismatch!"}
 
   def password
     @password ||= Password.new(password_hash)
